e1b24c3a2b5a15e4497af194e3a6ccfa1c078ba9
AVA <ala-vlad@mail.ru>
2018-05-23 13:55:26 +0700

    Исследовать до каких  s и t работает

--- gordon.cpp
+++ gordon.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <math.h>
 #include "headers/getI.h"
 #include "headers/gen.h"
 #include "headers/degree.h"
@@ -18,39 +19,59 @@ unsigned int test(int k)
     return num;
 }
 
-unsigned int gordon()
+unsigned int gordon(int k)
 {
-	int k=3+rand()%12;
-	unsigned s=test(k);
-	//std::cout<<"s= "<<s<<std::endl;
-	unsigned t=test(k);
-	//std::cout<<"t= "<<t<<std::endl;
-  unsigned i0=1+rand()%7;
-  //std::cout<<"i0= "<<i0<<std::endl;
-  t<<=1;
-  unsigned r=1;
-  do {
-    r+=t;
-  	//std::cout<<"r= "<<r<<std::endl;
-  } while(!miller_rabin(r, 3));
-	unsigned p0=2*(binaryDegree_RightToLeft(s,(r-2),r))*s-1;
-	//std::cout<<"p0= "<<p0<<std::endl;
-	unsigned j0=1+rand()%7;
-	//std::cout<<"j0= "<<j0<<std::endl;
-  s*=(r<<1);
-  unsigned long long p=p0;
-  do {
-      p+=s;
-  	  //std::cout<<"p= "<<p<<std::endl;
-  } while(!miller_rabin(p,3));
-	return p;
+  bool flag=true;
+  unsigned long long p0;
+  //unsigned long long max=pow(2,32);
+  do
+  {
+    unsigned s=test(k);
+  	//std::cout<<"s= "<<s<<std::endl;
+  	unsigned t=test(k);
+  	//std::cout<<"t= "<<t<<std::endl;
+    unsigned i0=1+rand()%7;
+    //std::cout<<"i0= "<<i0<<std::endl;
+    t<<=1;
+    unsigned r=t*i0+1;
+    do {
+      unsigned tmp=r;
+      r+=t;
+      if(r<tmp)
+      {
+        flag=false;
+      }
+    	//std::cout<<"r= "<<r<<std::endl;
+    } while(!miller_rabin(r, 3));
+  	p0=2*(binaryDegree_RightToLeft(s,(r-2),r))*s-1;
+  	//std::cout<<"p0= "<<p0<<std::endl;
+  	unsigned j0=1+rand()%7;
+  	//std::cout<<"j0= "<<j0<<std::endl;
+    s*=(r<<1);
+    p0+=(s*j0);
+    while(!miller_rabin(p0,3))
+    {
+      unsigned long long p=p0;
+      p0+=s;
+      if(p0<p)
+      {
+        flag=false;
+      }
+      //std::cout<<p0<<"  ";
+    }
+    std::cout<<std::endl;
+  	return p0;
+  }
+  while(flag);
 }
+
 
 int main()
 {
 	srand(time(NULL));
+  int k=3+rand()%12;
   for(int i=0; i<10;++i)
   {
-    std::cout<<i+1<<". "<<gordon()<<std::endl;
+    std::cout<<i+1<<". "<<gordon(k)<<std::endl;
   }
 }
